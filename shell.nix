c778283198f08a43cc56b2105287ce0de9b8aaf1
Hardy Jones <jones3.hardy@gmail.com>
2023-02-18 14:32:45 -0800

    Initial bootstrapping
    We setup just the very absolute minimum files and expand on the README
    just a touch. This should be enough to get us rolling.
    
    We use `nix` to boostrap everything, then `bazel` as the workhorse of
    the build. We'll probably want to add some simpler ways to build
    everything when it starts to get complex. But we have time before that
    happens.
    
    We also setup a GitHub Action to run a quick job that should validate
    that everything is working.
    
    Branch: joneshf/initial-bootstrapping
    Pull-Request: https://github.com/joneshf/terraform-provider-openwrt/pull/1

---
+++ shell.nix
@@ -0,0 +1,24 @@
+let
+
+  nixpkgs-tarball_22-11 = builtins.fetchTarball {
+    sha256 = "sha256:11w3wn2yjhaa5pv20gbfbirvjq6i3m7pqrq2msf0g7cv44vijwgw";
+    # 22.11
+    url = "https://github.com/NixOS/nixpkgs/archive/4d2b37a84fad1091b9de401eb450aae66f1a741e.tar.gz";
+  };
+
+in
+
+{ pkgs ? import nixpkgs-tarball_22-11 { } }:
+
+pkgs.mkShell {
+  nativeBuildInputs = [
+    pkgs.bazel_6
+    pkgs.bazel-buildtools
+    pkgs.gh
+    pkgs.git
+    pkgs.go
+    pkgs.go-tools
+    pkgs.nixpkgs-fmt
+    pkgs.terraform
+  ];
+}
