c7c3facb0447dea8d6a980d587bd4467f9ee4615
Hardy Jones <jones3.hardy@gmail.com>
2023-02-25 08:40:36 -0800

    Extract JSON-RPC implementation
    We pull this stuff out so the rest is a little easier to understand.
    It's very unfortunate that we even need to write this code, but the
    `net/jsonrpc` package doesn't work with more than one value in
    `"params"`, so here we are.
    
    Branch: joneshf/extract-json-rpc-implementation
    Pull-Request: https://github.com/joneshf/terraform-provider-openwrt/pull/8

--- lucirpc/client.go
+++ lucirpc/client.go
@@ -4,13 +4,15 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
 )
 
 const (
+	humanReadableGetSection = "get section"
+	humanReadableLogin      = "login"
+
 	methodGetAll = "get_all"
 	methodLogin  = "login"
 
@@ -21,8 +23,7 @@ const (
 )
 
 type Client struct {
-	addressUCI url.URL
-	client     *http.Client
+	jsonRPCClientUCI jsonRPCClient
 }
 
 func (c *Client) GetSection(
@@ -30,52 +31,25 @@ func (c *Client) GetSection(
 	config string,
 	section string,
 ) (map[string]string, error) {
-	requestBody := getSectionRequestBody{
+	requestBody := jsonRPCRequestBody{
 		Method: methodGetAll,
-		Params: [2]string{config, section},
+		Params: []string{config, section},
 	}
-	buffer := bytes.Buffer{}
-	encoder := json.NewEncoder(&buffer)
-	err := encoder.Encode(requestBody)
-	if err != nil {
-		return nil, fmt.Errorf("problem encoding get section request: %w", err)
-	}
-
-	request, err := http.NewRequestWithContext(
+	responseBody, err := c.jsonRPCClientUCI.Invoke(
 		ctx,
-		http.MethodPost,
-		c.addressUCI.String(),
-		&buffer,
+		humanReadableGetSection,
+		requestBody,
 	)
 	if err != nil {
-		return nil, fmt.Errorf("problem creating get section request: %w", err)
+		return nil, fmt.Errorf("unable to %s: %w", humanReadableGetSection, err)
 	}
 
-	response, err := c.client.Do(request)
+	var result map[string]string
+	err = json.Unmarshal(responseBody, &result)
 	if err != nil {
-		return nil, fmt.Errorf("problem sending request to get section: %w", err)
+		return nil, fmt.Errorf("unable to parse %s response: %w", humanReadableGetSection, err)
 	}
 
-	if response.StatusCode != 200 {
-		return nil, fmt.Errorf("expected get section to respond with a 200: got %s", response.Status)
-	}
-
-	var responseBody getSectionResponseBody
-	decoder := json.NewDecoder(response.Body)
-	err = decoder.Decode(&responseBody)
-	if err != nil {
-		return nil, fmt.Errorf("unable to parse get section response: %w", err)
-	}
-
-	if responseBody.Error != nil {
-		return nil, fmt.Errorf("unable to get section: %s", *responseBody.Error)
-	}
-
-	if responseBody.Result == nil {
-		return nil, errors.New("invalid get section response: expected either an error or a result, got neither")
-	}
-
-	result := *responseBody.Result
 	return result, nil
 }
 
@@ -98,52 +72,29 @@ func NewClient(
 		Scheme: scheme,
 	}
 	httpClient := &http.Client{}
-	requestBody := authRequestBody{
+	requestBody := jsonRPCRequestBody{
 		Method: methodLogin,
-		Params: [2]string{username, password},
+		Params: []string{username, password},
 	}
-	buffer := bytes.Buffer{}
-	encoder := json.NewEncoder(&buffer)
-	err := encoder.Encode(requestBody)
-	if err != nil {
-		return nil, fmt.Errorf("problem encoding login request: %w", err)
-	}
-
-	request, err := http.NewRequestWithContext(
+	jsonRPCClient := jsonRPCNewClient(
+		*httpClient,
+		address,
+	)
+	responseBody, err := jsonRPCClient.Invoke(
 		ctx,
-		http.MethodPost,
-		address.String(),
-		&buffer,
+		humanReadableLogin,
+		requestBody,
 	)
 	if err != nil {
-		return nil, fmt.Errorf("problem creating login request: %w", err)
+		return nil, fmt.Errorf("unable to %s: %w", humanReadableLogin, err)
 	}
 
-	response, err := httpClient.Do(request)
+	var authToken string
+	err = json.Unmarshal(responseBody, &authToken)
 	if err != nil {
-		return nil, fmt.Errorf("problem sending request to login: %w", err)
+		return nil, fmt.Errorf("unable to parse %s response: %w", humanReadableLogin, err)
 	}
 
-	if response.StatusCode != 200 {
-		return nil, fmt.Errorf("expected login to respond with a 200: got %s", response.Status)
-	}
-
-	var responseBody authResponseBody
-	decoder := json.NewDecoder(response.Body)
-	err = decoder.Decode(&responseBody)
-	if err != nil {
-		return nil, fmt.Errorf("unable to parse login response: %w", err)
-	}
-
-	if responseBody.Error != nil {
-		return nil, fmt.Errorf("unable to login: %s", *responseBody.Error)
-	}
-
-	if responseBody.Result == nil {
-		return nil, errors.New("invalid login response: expected either an error or a result, got neither")
-	}
-
-	authToken := *responseBody.Result
 	query := url.Values{}
 	query.Add(queryKeyAuth, authToken)
 	addressUCI := url.URL{
@@ -152,29 +103,86 @@ func NewClient(
 		RawQuery: query.Encode(),
 		Scheme:   scheme,
 	}
+	jsonRPCClientUCI := jsonRPCNewClient(
+		*httpClient,
+		addressUCI,
+	)
 	client := &Client{
-		addressUCI: addressUCI,
-		client:     httpClient,
+		jsonRPCClientUCI: jsonRPCClientUCI,
 	}
 	return client, nil
 }
 
-type authRequestBody struct {
-	Method string    `json:"method"`
-	Params [2]string `json:"params"`
+type jsonRPCClient struct {
+	address url.URL
+	client  http.Client
 }
 
-type authResponseBody struct {
-	Error  *string `json:"error"`
-	Result *string `json:"result"`
+func (c jsonRPCClient) Invoke(
+	ctx context.Context,
+	humanReadableMethod string,
+	requestBody jsonRPCRequestBody,
+) (json.RawMessage, error) {
+	buffer := bytes.Buffer{}
+	encoder := json.NewEncoder(&buffer)
+	err := encoder.Encode(requestBody)
+	if err != nil {
+		return nil, fmt.Errorf("problem encoding %s request: %w", humanReadableMethod, err)
+	}
+
+	request, err := http.NewRequestWithContext(
+		ctx,
+		http.MethodPost,
+		c.address.String(),
+		&buffer,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("problem creating %s request: %w", humanReadableMethod, err)
+	}
+
+	response, err := c.client.Do(request)
+	if err != nil {
+		return nil, fmt.Errorf("problem sending request to %s: %w", humanReadableMethod, err)
+	}
+
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("expected %s to respond with a 200: got %s", humanReadableMethod, response.Status)
+	}
+
+	var responseBody jsonRPCResponseBody
+	decoder := json.NewDecoder(response.Body)
+	err = decoder.Decode(&responseBody)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse %s response: %w", humanReadableMethod, err)
+	}
+
+	if responseBody.Error == nil && responseBody.Result == nil {
+		return nil, fmt.Errorf("invalid %s response: expected either an error or a result, got neither", humanReadableMethod)
+	}
+
+	if responseBody.Error != nil {
+		return nil, fmt.Errorf("%s error: %s", humanReadableMethod, *responseBody.Error)
+	}
+
+	return *responseBody.Result, nil
 }
 
-type getSectionRequestBody struct {
-	Method string    `json:"method"`
-	Params [2]string `json:"params"`
+func jsonRPCNewClient(
+	httpClient http.Client,
+	address url.URL,
+) jsonRPCClient {
+	return jsonRPCClient{
+		address: address,
+		client:  httpClient,
+	}
 }
 
-type getSectionResponseBody struct {
-	Error  *string            `json:"error"`
-	Result *map[string]string `json:"result"`
+type jsonRPCRequestBody struct {
+	Method string   `json:"method"`
+	Params []string `json:"params"`
+}
+
+type jsonRPCResponseBody struct {
+	Error  *string          `json:"error"`
+	Result *json.RawMessage `json:"result"`
 }
