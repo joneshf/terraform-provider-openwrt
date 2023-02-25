9392706ed3e32aee5ab19b2636b866b5deb26f4f
Hardy Jones <jones3.hardy@gmail.com>
2023-02-25 06:42:44 -0800

    Start working on an RPC client for LuCI
    Apparently, LuCI has an RPC client that can be used a little easier than
    `go-uci`: https://github.com/openwrt/luci/wiki/JsonRpcHowTo#basics. It
    requires installing a couple of packages, so it might not work for
    everyone. But, it'll work for us.
    
    We toss together a simple package that makes setting up and getting data
    from a section relatively straight-forward. We'll add more behavior as
    we need it. But, this is a decent start.
    
    Branch: joneshf/start-working-on-an-rpc-client-for-luci
    Pull-Request: https://github.com/joneshf/terraform-provider-openwrt/pull/7

---
+++ lucirpc/client.go
@@ -0,0 +1,180 @@
+package lucirpc
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+const (
+	methodGetAll = "get_all"
+	methodLogin  = "login"
+
+	pathAuth = "/cgi-bin/luci/rpc/auth"
+	pathUCI  = "/cgi-bin/luci/rpc/uci"
+
+	queryKeyAuth = "auth"
+)
+
+type Client struct {
+	addressUCI url.URL
+	client     *http.Client
+}
+
+func (c *Client) GetSection(
+	ctx context.Context,
+	config string,
+	section string,
+) (map[string]string, error) {
+	requestBody := getSectionRequestBody{
+		Method: methodGetAll,
+		Params: [2]string{config, section},
+	}
+	buffer := bytes.Buffer{}
+	encoder := json.NewEncoder(&buffer)
+	err := encoder.Encode(requestBody)
+	if err != nil {
+		return nil, fmt.Errorf("problem encoding get section request: %w", err)
+	}
+
+	request, err := http.NewRequestWithContext(
+		ctx,
+		http.MethodPost,
+		c.addressUCI.String(),
+		&buffer,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("problem creating get section request: %w", err)
+	}
+
+	response, err := c.client.Do(request)
+	if err != nil {
+		return nil, fmt.Errorf("problem sending request to get section: %w", err)
+	}
+
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("expected get section to respond with a 200: got %s", response.Status)
+	}
+
+	var responseBody getSectionResponseBody
+	decoder := json.NewDecoder(response.Body)
+	err = decoder.Decode(&responseBody)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse get section response: %w", err)
+	}
+
+	if responseBody.Error != nil {
+		return nil, fmt.Errorf("unable to get section: %s", *responseBody.Error)
+	}
+
+	if responseBody.Result == nil {
+		return nil, errors.New("invalid get section response: expected either an error or a result, got neither")
+	}
+
+	result := *responseBody.Result
+	return result, nil
+}
+
+func NewClient(
+	ctx context.Context,
+	scheme string,
+	hostname string,
+	port uint16,
+	username string,
+	password string,
+) (*Client, error) {
+	host := hostname
+	if port != 0 {
+		host = fmt.Sprintf("%s:%d", host, port)
+	}
+
+	address := url.URL{
+		Host:   host,
+		Path:   pathAuth,
+		Scheme: scheme,
+	}
+	httpClient := &http.Client{}
+	requestBody := authRequestBody{
+		Method: methodLogin,
+		Params: [2]string{username, password},
+	}
+	buffer := bytes.Buffer{}
+	encoder := json.NewEncoder(&buffer)
+	err := encoder.Encode(requestBody)
+	if err != nil {
+		return nil, fmt.Errorf("problem encoding login request: %w", err)
+	}
+
+	request, err := http.NewRequestWithContext(
+		ctx,
+		http.MethodPost,
+		address.String(),
+		&buffer,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("problem creating login request: %w", err)
+	}
+
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return nil, fmt.Errorf("problem sending request to login: %w", err)
+	}
+
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("expected login to respond with a 200: got %s", response.Status)
+	}
+
+	var responseBody authResponseBody
+	decoder := json.NewDecoder(response.Body)
+	err = decoder.Decode(&responseBody)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse login response: %w", err)
+	}
+
+	if responseBody.Error != nil {
+		return nil, fmt.Errorf("unable to login: %s", *responseBody.Error)
+	}
+
+	if responseBody.Result == nil {
+		return nil, errors.New("invalid login response: expected either an error or a result, got neither")
+	}
+
+	authToken := *responseBody.Result
+	query := url.Values{}
+	query.Add(queryKeyAuth, authToken)
+	addressUCI := url.URL{
+		Host:     host,
+		Path:     pathUCI,
+		RawQuery: query.Encode(),
+		Scheme:   scheme,
+	}
+	client := &Client{
+		addressUCI: addressUCI,
+		client:     httpClient,
+	}
+	return client, nil
+}
+
+type authRequestBody struct {
+	Method string    `json:"method"`
+	Params [2]string `json:"params"`
+}
+
+type authResponseBody struct {
+	Error  *string `json:"error"`
+	Result *string `json:"result"`
+}
+
+type getSectionRequestBody struct {
+	Method string    `json:"method"`
+	Params [2]string `json:"params"`
+}
+
+type getSectionResponseBody struct {
+	Error  *string            `json:"error"`
+	Result *map[string]string `json:"result"`
+}
---
+++ lucirpc/client_test.go
@@ -0,0 +1,432 @@
+package lucirpc_test
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strconv"
+	"testing"
+
+	"github.com/joneshf/terraform-provider-openwrt/lucirpc"
+	"gotest.tools/v3/assert"
+)
+
+func TestClientGetSection(t *testing.T) {
+	t.Run("handles server not existing", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+		}
+		client, close := authenticatedClient(
+			t,
+			ctx,
+			http.HandlerFunc(handle),
+		)
+		close()
+
+		// When
+		_, err := client.GetSection(
+			ctx,
+			"",
+			"",
+		)
+
+		// Then
+		assert.ErrorContains(t, err, "problem sending request to get section")
+	})
+
+	t.Run("makes a request to correct endpoint", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+			switch r.URL.Path {
+			case "/cgi-bin/luci/rpc/uci":
+				fmt.Fprintf(w, `{
+					"result": {}
+				}`)
+
+			default:
+				w.WriteHeader(http.StatusNotFound)
+			}
+		}
+		client, close := authenticatedClient(
+			t,
+			ctx,
+			http.HandlerFunc(handle),
+		)
+		defer close()
+
+		// When
+		_, err := client.GetSection(
+			ctx,
+			"",
+			"",
+		)
+
+		// Then
+		assert.NilError(t, err)
+	})
+
+	t.Run("expects a 200 response", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusAccepted)
+		}
+		client, close := authenticatedClient(
+			t,
+			ctx,
+			http.HandlerFunc(handle),
+		)
+		defer close()
+
+		// When
+		_, err := client.GetSection(
+			ctx,
+			"",
+			"",
+		)
+
+		// Then
+		assert.ErrorContains(t, err, "expected get section to respond with a 200")
+	})
+
+	t.Run("expects a valid JSON-RPC response", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, `[]`)
+		}
+		client, close := authenticatedClient(
+			t,
+			ctx,
+			http.HandlerFunc(handle),
+		)
+		defer close()
+
+		// When
+		_, err := client.GetSection(
+			ctx,
+			"",
+			"",
+		)
+
+		// Then
+		assert.ErrorContains(t, err, "unable to parse get section response")
+	})
+
+	t.Run("returns error when get section fails", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, `{
+				"error": ""
+			}`)
+		}
+		client, close := authenticatedClient(
+			t,
+			ctx,
+			http.HandlerFunc(handle),
+		)
+		defer close()
+
+		// When
+		_, err := client.GetSection(
+			ctx,
+			"",
+			"",
+		)
+
+		// Then
+		assert.ErrorContains(t, err, "unable to get section")
+	})
+
+	t.Run("handles invalid response", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "{}")
+		}
+		client, close := authenticatedClient(
+			t,
+			ctx,
+			http.HandlerFunc(handle),
+		)
+		defer close()
+
+		// When
+		_, err := client.GetSection(
+			ctx,
+			"",
+			"",
+		)
+
+		// Then
+		assert.ErrorContains(t, err, "invalid get section response")
+	})
+
+	t.Run("returns section data when successful", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, `{
+				"result": {
+					".name": "section-name",
+					"baz": "1",
+					"foo": "bar"
+				}
+			}`)
+		}
+		client, close := authenticatedClient(
+			t,
+			ctx,
+			http.HandlerFunc(handle),
+		)
+		defer close()
+
+		// When
+		got, err := client.GetSection(
+			ctx,
+			"",
+			"",
+		)
+
+		// Then
+		assert.NilError(t, err)
+		want := map[string]string{
+			".name": "section-name",
+			"baz":   "1",
+			"foo":   "bar",
+		}
+		assert.DeepEqual(t, got, want)
+	})
+}
+
+func TestNewClient(t *testing.T) {
+	t.Run("handles server not existing", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+
+		// When
+		_, err := lucirpc.NewClient(
+			ctx,
+			"http",
+			"non.existent",
+			80,
+			"root",
+			"",
+		)
+
+		// Then
+		assert.ErrorContains(t, err, "problem sending request to login")
+	})
+
+	t.Run("makes request to correct endpoint", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+			switch r.URL.Path {
+			case "/cgi-bin/luci/rpc/auth":
+				fmt.Fprintf(w, `{
+					"result": "correct path"
+				}`)
+
+			default:
+				w.WriteHeader(http.StatusNotFound)
+			}
+		}
+		address, port, close := newServer(t, http.HandlerFunc(handle))
+		defer close()
+
+		// When
+		_, err := lucirpc.NewClient(
+			ctx,
+			address.Scheme,
+			address.Hostname(),
+			uint16(port),
+			"root",
+			"",
+		)
+
+		// Then
+		assert.NilError(t, err)
+	})
+
+	t.Run("expects a 200 response", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusAccepted)
+		}
+		address, port, close := newServer(t, http.HandlerFunc(handle))
+		defer close()
+
+		// When
+		_, err := lucirpc.NewClient(
+			ctx,
+			address.Scheme,
+			address.Hostname(),
+			uint16(port),
+			"root",
+			"",
+		)
+
+		// Then
+		assert.ErrorContains(t, err, "expected login to respond with a 200")
+	})
+
+	t.Run("expects a valid JSON-RPC response", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "[]")
+		}
+		address, port, close := newServer(t, http.HandlerFunc(handle))
+		defer close()
+
+		// When
+		_, err := lucirpc.NewClient(
+			ctx,
+			address.Scheme,
+			address.Hostname(),
+			uint16(port),
+			"root",
+			"",
+		)
+
+		// Then
+		assert.ErrorContains(t, err, "unable to parse login response")
+	})
+
+	t.Run("returns error when authentication fails", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, `{
+				"error": "invalid password"
+			}`)
+		}
+		address, port, close := newServer(t, http.HandlerFunc(handle))
+		defer close()
+
+		// When
+		_, err := lucirpc.NewClient(
+			ctx,
+			address.Scheme,
+			address.Hostname(),
+			uint16(port),
+			"root",
+			"",
+		)
+
+		// Then
+		assert.ErrorContains(t, err, "unable to login")
+	})
+
+	t.Run("handles invalid responses", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "{}")
+		}
+		address, port, close := newServer(t, http.HandlerFunc(handle))
+		defer close()
+
+		// When
+		_, err := lucirpc.NewClient(
+			ctx,
+			address.Scheme,
+			address.Hostname(),
+			uint16(port),
+			"root",
+			"",
+		)
+
+		// Then
+		assert.ErrorContains(t, err, "invalid login response")
+	})
+
+	t.Run("does not error when authentication succeeds", func(t *testing.T) {
+		// Given
+		ctx := context.Background()
+		handle := func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, `{
+				"result": "abc123"
+			}`)
+		}
+		address, port, close := newServer(t, http.HandlerFunc(handle))
+		defer close()
+
+		// When
+		_, err := lucirpc.NewClient(
+			ctx,
+			address.Scheme,
+			address.Hostname(),
+			uint16(port),
+			"root",
+			"",
+		)
+
+		// Then
+		assert.NilError(t, err)
+	})
+}
+
+func authenticatedClient(
+	t *testing.T,
+	ctx context.Context,
+	handler http.Handler,
+) (*lucirpc.Client, func()) {
+	t.Helper()
+	handleWithAuth := func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/cgi-bin/luci/rpc/auth":
+			fmt.Fprintf(w, `{
+					"result": "abc123"
+				}`)
+		default:
+			handler.ServeHTTP(w, r)
+		}
+	}
+	address, port, close := newServer(
+		t,
+		http.HandlerFunc(handleWithAuth),
+	)
+	client, err := lucirpc.NewClient(
+		ctx,
+		address.Scheme,
+		address.Hostname(),
+		uint16(port),
+		"root",
+		"",
+	)
+	if err != nil {
+		close()
+		assert.NilError(t, err)
+	}
+
+	return client, close
+}
+
+func newServer(
+	t *testing.T,
+	handler http.Handler,
+) (*url.URL, int, func()) {
+	t.Helper()
+	server := httptest.NewServer(handler)
+	address, err := url.Parse(server.URL)
+	if err != nil {
+		server.Close()
+		assert.NilError(t, err)
+	}
+
+	port, err := strconv.Atoi(address.Port())
+	if err != nil {
+		server.Close()
+		assert.NilError(t, err)
+	}
+
+	return address, port, server.Close
+}
