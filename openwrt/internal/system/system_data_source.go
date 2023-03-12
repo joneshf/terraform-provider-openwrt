ed2f57d745c0fb597279c36bdbb76566caecdbd8
Hardy Jones <jones3.hardy@gmail.com>
2023-03-11 20:56:55 -0800

    Abstract the behavior of Data Sources
    Since all of the Data Sources are doing the same stuff, we pull out the
    behavior to a common location that we can use in multiple places. We
    then initialize it with the pieces that are different: a getter for the
    id, the schema stuff, and the UCI stuff.
    
    Then, we can simplify the actual Data Sources to almost nothing. We
    still keep them around because testing their behavior is different for
    each one. But this might be as minimal as we can get things.
    
    Branch: joneshf/abstract-the-behavior-of-data-sources
    Pull-Request: https://github.com/joneshf/terraform-provider-openwrt/pull/76

--- openwrt/internal/system/system_data_source.go
+++ openwrt/internal/system/system_data_source.go
@@ -1,115 +1,16 @@
 package system
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
-	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
-	"github.com/joneshf/terraform-provider-openwrt/lucirpc"
 	"github.com/joneshf/terraform-provider-openwrt/openwrt/internal/lucirpcglue"
 )
 
-var (
-	_ datasource.DataSource              = &systemDataSource{}
-	_ datasource.DataSourceWithConfigure = &systemDataSource{}
-)
-
 func NewSystemDataSource() datasource.DataSource {
-	return &systemDataSource{}
-}
-
-type systemDataSource struct {
-	client        lucirpc.Client
-	fullTypeName  string
-	terraformType string
-}
-
-// Configure prepares the data source.
-func (d *systemDataSource) Configure(
-	ctx context.Context,
-	req datasource.ConfigureRequest,
-	res *datasource.ConfigureResponse,
-) {
-	tflog.Info(ctx, fmt.Sprintf("Configuring %s Data Source", d.fullTypeName))
-	if req.ProviderData == nil {
-		tflog.Debug(ctx, "No provider data")
-		return
-	}
-
-	client, diagnostics := lucirpcglue.NewClient(lucirpcglue.ConfigureRequest(req))
-	res.Diagnostics.Append(diagnostics...)
-	if res.Diagnostics.HasError() {
-		return
-	}
-
-	d.client = *client
-}
-
-// Metadata sets the data source name.
-func (d *systemDataSource) Metadata(
-	ctx context.Context,
-	req datasource.MetadataRequest,
-	res *datasource.MetadataResponse,
-) {
-	fullTypeName := fmt.Sprintf("%s_%s", req.ProviderTypeName, systemTypeName)
-	d.fullTypeName = fullTypeName
-	d.terraformType = lucirpcglue.DataSourceTerraformType
-	res.TypeName = fullTypeName
-}
-
-// Read prepares the data source.
-func (d *systemDataSource) Read(
-	ctx context.Context,
-	req datasource.ReadRequest,
-	res *datasource.ReadResponse,
-) {
-	tflog.Info(ctx, fmt.Sprintf("Reading %s data source", d.fullTypeName))
-
-	tflog.Debug(ctx, "Retrieving values from config")
-	var model systemModel
-	diagnostics := req.Config.Get(ctx, &model)
-	res.Diagnostics.Append(diagnostics...)
-	if res.Diagnostics.HasError() {
-		return
-	}
-
-	ctx, model, diagnostics = lucirpcglue.ReadModel(
-		ctx,
-		d.fullTypeName,
-		d.terraformType,
-		d.client,
+	return lucirpcglue.NewDataSource(
+		systemModelGetId,
 		systemSchemaAttributes,
+		systemSchemaDescription,
 		systemUCIConfig,
-		model.Id.ValueString(),
+		systemUCISystemType,
 	)
-	res.Diagnostics.Append(diagnostics...)
-	if res.Diagnostics.HasError() {
-		return
-	}
-
-	tflog.Debug(ctx, fmt.Sprintf("Setting the %s data source state", d.fullTypeName))
-	diagnostics = res.State.Set(ctx, model)
-	res.Diagnostics.Append(diagnostics...)
-	if res.Diagnostics.HasError() {
-		return
-	}
-}
-
-// Schema prepares the data source.
-func (d *systemDataSource) Schema(
-	ctx context.Context,
-	req datasource.SchemaRequest,
-	res *datasource.SchemaResponse,
-) {
-	attributes := map[string]schema.Attribute{}
-	for k, v := range systemSchemaAttributes {
-		attributes[k] = v.ToDataSource()
-	}
-
-	res.Schema = schema.Schema{
-		Attributes:  attributes,
-		Description: systemSchemaDescription,
-	}
 }
