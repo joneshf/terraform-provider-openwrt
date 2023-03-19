d8a839ca7806857eb412c7c5601e765c06f927ed
joneshf <jones3.hardy@gmail.com>
2023-03-19 10:50:43 -0700

    Test the marshal/unmarshal behavior as well
    We want to have some decent confidence that we can parse and serialize
    the JSON we get back from LuCI's JSON-RPC API. We toss a couple of tests
    together to validate that behavior.

--- lucirpc/options_test.go
+++ lucirpc/options_test.go
@@ -1,6 +1,7 @@
 package lucirpc_test
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/joneshf/terraform-provider-openwrt/lucirpc"
@@ -184,5 +185,96 @@ func TestOptionsGetString(t *testing.T) {
 		want := "hello"
 		assert.NilError(t, err)
 		assert.DeepEqual(t, got, want)
+	})
+}
+
+func TestOptionsMarshalJSON(t *testing.T) {
+	t.Run("works for all types", func(t *testing.T) {
+		// Given
+		options := lucirpc.Options{
+			"option1": lucirpc.Boolean(true),
+			"option2": lucirpc.Boolean(false),
+			"option3": lucirpc.Integer(31),
+			"option4": lucirpc.String("hello"),
+			"option5": lucirpc.ListString([]string{
+				"value1",
+				"value2",
+				"value3",
+			}),
+		}
+
+		// When
+		got, err := json.MarshalIndent(options, "\t\t", "\t")
+
+		// Then
+		want := []byte(`{
+			"option1": true,
+			"option2": false,
+			"option3": 31,
+			"option4": "hello",
+			"option5": [
+				"value1",
+				"value2",
+				"value3"
+			]
+		}`)
+		assert.NilError(t, err)
+		assert.DeepEqual(t, got, want)
+	})
+}
+
+func TestOptionsUnmarshalJSON(t *testing.T) {
+	t.Run("works for all types", func(t *testing.T) {
+		// Given
+		var options lucirpc.Options
+		rawJSON := `{
+			"option1": true,
+			"option2": "1",
+			"option3": "yes",
+			"option4": "on",
+			"option5": "true",
+			"option6": "enabled",
+			"option7": false,
+			"option8": "0",
+			"option9": "no",
+			"option10": "off",
+			"option11": "false",
+			"option12": "disabled",
+			"option13": "31",
+			"option14": "hello",
+			"option15": [
+				"value1",
+				"value2",
+				"value3"
+			]
+		}`
+
+		// When
+		err := json.Unmarshal([]byte(rawJSON), &options)
+
+		// Then
+		want := lucirpc.Options{
+			"option1":  lucirpc.Boolean(true),
+			"option2":  lucirpc.Boolean(true),
+			"option3":  lucirpc.Boolean(true),
+			"option4":  lucirpc.Boolean(true),
+			"option5":  lucirpc.Boolean(true),
+			"option6":  lucirpc.Boolean(true),
+			"option7":  lucirpc.Boolean(false),
+			"option8":  lucirpc.Boolean(false),
+			"option9":  lucirpc.Boolean(false),
+			"option10": lucirpc.Boolean(false),
+			"option11": lucirpc.Boolean(false),
+			"option12": lucirpc.Boolean(false),
+			"option13": lucirpc.Integer(31),
+			"option14": lucirpc.String("hello"),
+			"option15": lucirpc.ListString([]string{
+				"value1",
+				"value2",
+				"value3",
+			}),
+		}
+		assert.NilError(t, err)
+		assert.DeepEqual(t, options, want)
 	})
 }
